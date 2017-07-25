c97984f658de4410e7ebdd4ba43bfcade92adfd9
Jake Robers <jakerobers@gmail.com>
2017-07-25 15:31:49 -0500

    Fix error

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+import os
+import sqlite3
 from flask import Flask, render_template, session, redirect, url_for, escape, request
-app = Flask(__name__)
 
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'chigwell-admin.db'),
+    SECRET_KEY='development key',# TODO: Refactor to another config.
+    USERNAME='admin',# TODO: Refactor to another config.
+    PASSWORD='default' # TODO: Refactor to another config.
+    ))
+
+app.config.from_envvar('ADMIN_SETTINGS', silent=True)
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
